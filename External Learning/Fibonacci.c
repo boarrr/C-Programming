6a3559c876aefc354257b9f8edd66b15cab7b54f
boarrr <inomoncatnip@hotmail.com>
2023-10-01 10:52:26 +0100

    Created a program to display Fibonacci numbers

---
+++ External Learning/Fibonacci.c
@@ -0,0 +1,35 @@
+/*
+  Author: Ryan Pitman
+  Description: A program to display Fibonacci to the 10th term
+  Date: 01/10/2023
+*/
+
+#include <stdio.h>
+
+void fib(int n);
+
+int main()
+{
+  int term = 10;
+
+  printf("\nDisplaying Fibonacci to the 10th term:\n");
+  fib(term);
+  
+  return 0;
+}
+
+void fib(int n)
+{
+  int x = 0, y = 1;
+  int z = 0;
+
+  while (n > 0)
+  {
+    printf("%d\n", x);
+    z = x + y;
+    x = y;
+    y = z;
+    
+    n--;
+  }
+} 
