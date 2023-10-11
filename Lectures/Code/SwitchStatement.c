348a1921fc6d535085efb617057652315f2cb369
boarrr <inomoncatnip@hotmail.com>
2023-10-11 15:03:39 +0100

    Updated Switch

--- Lectures/Code/SwitchStatement.c
+++ Lectures/Code/SwitchStatement.c
@@ -17,41 +17,36 @@ int main()
   switch(oper)
   {
     case '+': // if (oper == '+' )
-    {
       answer = num1 + num2;
       printf("\n%.1f PLUS %.1f is %.1f\n", num1, num2, answer);
       break;
-    } // end case '+'
+    // end case '+'
 
     case '-':
-    {
       answer = num1 - num2;
       printf("\n%.1f MINUS %.1f is %.1f\n", num1, num2, answer);
       break;
-    } // end case '-'
+    // end case '-'
 
     case '*':
     case 'x':
     case 'X':
-    // similar to: if (oper == '*' || oper == 'x' || oper == 'X')
-    {
+      // similar to: if (oper == '*' || oper == 'x' || oper == 'X')
       answer = num1 * num2;
       printf("\n%.1f MULTIPLY %.1f is %.1f\n", num1, num2, answer);
       break;
-    } // end case '*'
+     // end case '*'
 
     case '/':
-    {
       answer = num1 / num2;
       printf("\n%.1f DIVIDE %.1f is %.1f\n", num1, num2, answer);
       break;
-    } // end case '/'
+    // end case '/'
 
     default:
-    {
       printf("\nInvalid operator entered\n");
       break;
-    } // end default
+    // end default
   } // end switch
 
   return 0;
