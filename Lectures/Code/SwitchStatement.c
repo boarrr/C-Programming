1a1a075658a983d9686bd4978f37b7c8d363ba40
boarrr <inomoncatnip@hotmail.com>
2023-10-11 15:12:30 +0100

    Updated switch code

--- Lectures/Code/SwitchStatement.c
+++ Lectures/Code/SwitchStatement.c
@@ -16,36 +16,46 @@ int main()
   // Now use the switch statement
   switch(oper)
   {
-    case '+': // if (oper == '+' )
+    case '+':
+    { // if (oper == '+' )
       answer = num1 + num2;
       printf("\n%.1f PLUS %.1f is %.1f\n", num1, num2, answer);
       break;
+    }
     // end case '+'
 
     case '-':
+    {
       answer = num1 - num2;
       printf("\n%.1f MINUS %.1f is %.1f\n", num1, num2, answer);
       break;
+    }
     // end case '-'
 
     case '*':
     case 'x':
     case 'X':
+    {
       // similar to: if (oper == '*' || oper == 'x' || oper == 'X')
       answer = num1 * num2;
       printf("\n%.1f MULTIPLY %.1f is %.1f\n", num1, num2, answer);
       break;
+    }
      // end case '*'
 
     case '/':
+    {
       answer = num1 / num2;
       printf("\n%.1f DIVIDE %.1f is %.1f\n", num1, num2, answer);
       break;
+    }
     // end case '/'
 
     default:
+    {
       printf("\nInvalid operator entered\n");
       break;
+    }
     // end default
   } // end switch
 
